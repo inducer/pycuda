6a998669e943656b8bfb02f372897ff01b32868f
Andreas Kloeckner <andreas@dart.ath.cx>
2008-05-07 20:57:10 -0400

    Add numpy-like interface to cublas.

--- test/test.py
+++ test/test.py
@@ -6,20 +6,11 @@ def main():
     import pyublas
 
     import numpy
-    shape = (1000000,)
-    a = numpy.ones(shape, dtype=numpy.float32)
-    b = 33*numpy.ones(shape, dtype=numpy.float32)
-    result = numpy.empty(shape, dtype=numpy.float32)
+    shape = (10,)
+    a = blas.ones(shape, dtype=numpy.float32)
+    b = 33*blas.ones(shape, dtype=numpy.float32)
 
-    a_gpu = blas.DevicePtrFloat32(shape[0])
-    a_gpu.set(a)
-    b_gpu = blas.DevicePtrFloat32(shape[0])
-    b_gpu.set(b)
-
-    blas.axpy(shape[0], 1, a_gpu, 1, b_gpu, 1)
-
-    b_gpu.get(result)
-    print result[:10], result[-10:]
+    print -a+b
 
 
 
