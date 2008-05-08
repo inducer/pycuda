2fd8199012836f7c6328d978da733802ed44a75a
Andreas Kloeckner <inform@tiker.net>
2008-05-08 13:37:17 -0400

    Add SourceModule.

--- src/python/driver.py
+++ src/python/driver.py
@@ -16,6 +16,11 @@ def _add_functionality():
 
 
 
+_add_functionality()
+
+
+
+
 def pagelocked_zeros(shape, dtype, order="C"):
     result = pagelocked_empty(shape, dtype, order)
     result.fill(0)
@@ -24,4 +29,32 @@ def pagelocked_zeros(shape, dtype, order="C"):
 
 
 
-_add_functionality()
+class SourceModule(object):
+    def __init__(self, source, options=[], keep=False):
+        from tempfile import mkdtemp
+        tempdir = mkdtemp()
+        print tempdir
+
+        from os.path import join
+        outf = open(join(tempdir, "kernel.cu"), "w")
+        outf.write(source)
+        outf.close()
+
+        from subprocess import call
+        result = call(["nvcc", "--cubin"] 
+                + options
+                + ["kernel.cu"]
+            cwd=tempdir)
+
+        data = open(join(tempdir, "kernel.cubin"), "r").read()
+        self.module = module_from_buffer(data)
+
+        if not keep:
+            from os import listdir, unlink, rmdir
+            for name in listdir(tempdir):
+                os.unlink(join(tempdir, name))
+            os.rmdir(tempdir)
+
+        self.get_function = self.module.get_function
+        self.get_global = self.module.get_global
+        self.get_texref = self.module.get_texref
