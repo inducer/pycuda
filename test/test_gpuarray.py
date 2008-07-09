00a32972bbbbfcd0a7338307438ac7e5a42df55d
wohlgemuth <wohlgemuth@ucdavis.edu>
2008-07-08 17:38:13 -0700

    added some other tests

--- test/test_gpuarray.py
+++ test/test_gpuarray.py
@@ -5,192 +5,20 @@ import numpy.linalg as la
 import unittest
 import pycuda.gpuarray as gpuarray
 import sys
+import test_abstract_array as test
 
-drv.init()
-ctx = drv.Device(0).make_context()
-gpuarray.GPUArray.compile_kernels()
+#initialize the device and the driver
+if not drv.was_context_created():
+    drv.init()
+    ctx = drv.Device(0).make_context()
+    gpuarray.GPUArray.compile_kernels()
 
-class TestGPUArray(unittest.TestCase):                                      
+class TestGPUArray(test.TestAbstractArray):                                      
     """tests the gpu array class"""
 
-    def test_multiply(self):
-        """Tests the muliplication of an array with a scalar. """
-
-        #test data
-        a = numpy.array([1,2,3,4,5,6,7,8,9,10]).astype(numpy.float32)
-
-        #convert a to a gpu object
-        a_gpu = gpuarray.to_gpu(a)
-
-        #multipy all elements in a_gpu * 2, this should run on the gpu
-        a_doubled = (2*a_gpu).get()
-
-        #check that the result is like we expsect it
-        for i in range(0,a.size):
-           self.assertEqual(a[i] * 2,a_doubled[i])
-
-
-    def test_multiply_array(self):
-        """Tests the multiplaction of two arrays."""
-
-        #test data
-        a = numpy.array([1,2,3,4,5,6,7,8,9,10]).astype(numpy.float32)
-
-        #convert a to a gpu object
-        a_gpu = gpuarray.to_gpu(a)
-        b_gpu = gpuarray.to_gpu(a)
-
-        #multipy all elements in a_gpu * b_gpu, this should run on the gpu
-        a_doubled = (b_gpu*a_gpu).get()
-
-        #check that the result is like we expsect it
-        for i in range(0,a.size):
-           self.assertEqual(a[i] * a[i],a_doubled[i])
-
-
-    def test_addition_array(self):
-        """Tests the addition of two arrays."""
-
-        #test data
-        a = numpy.array([1,2,3,4,5,6,7,8,9,10]).astype(numpy.float32)
-
-        #convert a to a gpu object
-        a_gpu = gpuarray.to_gpu(a)
-        b_gpu = gpuarray.to_gpu(a)
-
-        #add all elements in a_gpu with b_gpu, this should run on the gpu
-        a_added = (b_gpu+a_gpu).get()
-
-        #check that the result is like we expsect it
-        for i in range(0,a.size):
-           self.assertEqual(a[i] + a[i],a_added[i])
-
-
-    def test_addition_scalar(self):
-        """Tests the addition of an array and a scalar."""
-
-        #test data
-        a = numpy.array([1,2,3,4,5,6,7,8,9,10]).astype(numpy.float32)
-
-        #convert a to a gpu object
-        a_gpu = gpuarray.to_gpu(a)
-
-        #add all elements in a_gpu to 7, this should run on the gpu
-        a_added = (7+a_gpu).get()
-
-        #check that the result is like we expsect it
-        for i in range(0,a.size):
-           self.assertEqual(7 + a[i],a_added[i])
-
-
-        a_added = (a_gpu + 7).get()
-
-        #check that the result is like we expsect it
-        for i in range(0,a.size):
-           self.assertEqual(7 + a[i],a_added[i])
-
-
-    def test_substract_array(self):
-        """Tests the substraction of two arrays."""
-        #test data
-        a = numpy.array([1,2,3,4,5,6,7,8,9,10]).astype(numpy.float32)
-        b = numpy.array([10,20,30,40,50,60,70,80,90,100]).astype(numpy.float32)
-
-        #convert a to a gpu object
-        a_gpu = gpuarray.to_gpu(a)
-        b_gpu = gpuarray.to_gpu(b)
-
-        #add all elements in a_gpu with b_gpu, this should run on the gpu
-        a_substract = (a_gpu-b_gpu).get()
-
-        #check that the result is like we expsect it
-        for i in range(0,a.size):
-           self.assertEqual(a[i] - b[i],a_substract[i])
-
-        #add all elements in a_gpu with b_gpu, this should run on the gpu
-        a_substract = (b_gpu-a_gpu).get()
-
-        #check that the result is like we expsect it
-        for i in range(0,a.size):
-           self.assertEqual(b[i] - a[i],a_substract[i])
-
-
-
-    def test_substract_scalar(self):
-        """Tests the substraction of an array and a scalar."""
-
-        #test data
-        a = numpy.array([1,2,3,4,5,6,7,8,9,10]).astype(numpy.float32)
-
-        #convert a to a gpu object
-        a_gpu = gpuarray.to_gpu(a)
-
-        #substract from all elements 7 in a_gpu
-        a_substract = (a_gpu-7).get()
-
-        #check that the result is like we expsect it
-        for i in range(0,a.size):
-           self.assertEqual(a[i]-7,a_substract[i])
-
-        #substract 7 from all elements in a_gpu
-        a_substract = (7-a_gpu).get()
-
-        #check that the result is like we expsect it
-        for i in range(0,a.size):
-           self.assertEqual(7-a[i],a_substract[i])
-
-
-
-
-    def test_divide_scalar(self):
-        """Tests the division of an array and a scalar."""
-
-        #test data
-        a = numpy.array([1,2,3,4,5,6,7,8,9,10]).astype(numpy.float32)
-
-        #convert a to a gpu object
-        a_gpu = gpuarray.to_gpu(a)
-
-        #divides the array by 2
-        a_divide = (a_gpu/2).get()
-
-        #check that the result is like we expsect it
-        for i in range(0,a.size):
-            self.assert_( abs(a[i]/2 - a_divide[i]) < 1e-3 )
-
-        #divides the array by 2
-        a_divide = (2/a_gpu).get()
-
-        #check that the result is like we expsect it
-        for i in range(0,a.size):
-            self.assert_( abs(2/a[i] - a_divide[i]) < 1e-3 )
-
-    def test_divide_array(self):
-        """Tests the division of an array and a scalar. """
-
-        #test data
-        a = numpy.array([10,20,30,40,50,60,70,80,90,100]).astype(numpy.float32)
-        b = numpy.array([10,10,10,10,10,10,10,10,10,10]).astype(numpy.float32)
-
-        #convert a to a gpu object
-        a_gpu = gpuarray.to_gpu(a)
-        b_gpu = gpuarray.to_gpu(b)
-
-        #divides the array
-        a_divide = (a_gpu/b_gpu).get()
-
-        #check that the result is like we expsect it
-        for i in range(0,a.size):
-            self.assert_( abs(a[i]/b[i] - a_divide[i]) < 1e-3 )
-
-        #divides the array
-        a_divide = (b_gpu/a_gpu).get()
-
-        #check that the result is like we expsect it
-        for i in range(0,a.size):
-           self.assert_( abs(b[i]/a[i] - a_divide[i]) < 1e-3 )
-
+    def create_array(self,array):
+        """creates a gpu array"""
+        return gpuarray.to_gpu(array)
 
 if __name__ == '__main__':
     unittest.main()
-
--- test/test_simplearray.py
+++ test/test_simplearray.py
@@ -11,5 +11,21 @@ class TestSimpleArray(test.TestAbstractArray):
         """creates an array an can be overwritten to replace the implementation"""
         return simplearray.to_gpu(array)
 
+    def test_matrix(self):
+        #initialize data with 0 of a size 100x100
+        a = simplearray.matrix(100,100)
+        
+        #initialize data with 20 of a size 100x100
+        b = simplearray.matrix(100,100,20)
+        
+        #initialize data with 30 of a size 100x100
+        c = simplearray.matrix(100,100,30)
+        
+        #simple formula to run over all the data
+        d = (a - 5) + (b + 2) / c
+        
+        for i in a:
+            print i
+        
 if __name__ == '__main__':
     unittest.main()
