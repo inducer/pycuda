cedf553c85129a21606298f45ed3a83be3c2a7e4
wohlgemuth <wohlgemuth@ucdavis.edu>
2008-07-12 01:29:10 -0700

    increased sample size for the test run and results still seem to match with the exspected data

--- test/test_math.py
+++ test/test_math.py
@@ -3,58 +3,63 @@ import unittest
 import pycuda.cumath as cumath
 import math as math
 
+test_sample =  1<<15
+    
+print "sample size: ", test_sample
+
 class TestMath(unittest.TestCase):
+    
     
     def test_ceil(self):
         """tests if the ceil function works"""
-        a = simplearray.array(100).fill_arange()/10        
+        a = simplearray.array(test_sample).fill_arange()/10        
         b = cumath.ceil(a)
         
-        for i in range(100):
+        for i in range(test_sample):
             self.assert_(math.ceil(a[i]) == b[i])
                
     def test_fabs(self):
         """tests if the fabs function works"""
-        a = simplearray.array(100).fill_arange() * -1    
+        a = simplearray.array(test_sample).fill_arange() * -1    
         b = cumath.fabs(a)
         
-        for i in range(100):
+        for i in range(test_sample):
             self.assert_(a[i] + b[i] == 0)
             self.assert_(b[i] >= 0)
                
     def test_floor(self):
         """tests if the floor function works"""
-        a = simplearray.array(100).fill_arange()/10        
+        a = simplearray.array(test_sample).fill_arange()/10        
         b = cumath.floor(a)
         
-        for i in range(100):
+        for i in range(test_sample):
             self.assert_(math.floor(a[i]) == b[i])
                
     def test_fmod(self):
         """tests if the fmod function works"""
-        a = simplearray.array(100).fill_arange()/10        
+        a = simplearray.array(test_sample).fill_arange()/10        
         b = cumath.fmod(a,2)
         
-        for i in range(100):
+        for i in range(test_sample):
             self.assert_(math.fmod(a[i],2) == b[i])
 
     def test_ldexp(self):
         """tests if the ldexp function works"""
-        a = simplearray.array(100).fill_arange()       
+        a = simplearray.array(test_sample).fill_arange()       
         b = cumath.ldexp(a,5)
         
-        for i in range(100):
+        for i in range(test_sample):
             self.assert_(math.ldexp(a[i],5) == b[i])
                
     def test_modf(self):
         """tests if the modf function works"""
-        a = simplearray.array(100).fill_arange()/10  
+        a = simplearray.array(test_sample).fill_arange()/10  
         b = cumath.modf(a)
     
         first = b[0]
         second = b[1]    
         
-        for i in range(100):
+        for i in range(test_sample):
             c = math.modf(a[i])
             
             self.assert_(c[0] == first[i])
@@ -62,13 +67,13 @@ class TestMath(unittest.TestCase):
                          
     def test_frexp(self):
         """tests if the frexp function works"""
-        a = simplearray.array(100).fill_arange()/10  
+        a = simplearray.array(test_sample).fill_arange()/10  
         b = cumath.frexp(a)
     
         first = b[0]
         second = b[1]    
         
-        for i in range(100):
+        for i in range(test_sample):
             c = math.frexp(a[i])
             
             self.assert_(c[0] == first[i])
@@ -84,18 +89,18 @@ class TestMath(unittest.TestCase):
 
     def test_log(self):
         """tests if the log function works"""
-        a = simplearray.array(100).fill_arange()+1       
+        a = simplearray.array(test_sample).fill_arange()+1       
         b = cumath.log(a)
         
-        for i in range(100):
+        for i in range(test_sample):
             self.assert_(abs(math.log(a[i]) - b[i]) < 1e-3)
 
     def test_log10(self):
         """tests if the log function works"""
-        a = simplearray.array(100).fill_arange()+1       
+        a = simplearray.array(test_sample).fill_arange()+1       
         b = cumath.log10(a)
         
-        for i in range(100):
+        for i in range(test_sample):
             self.assert_(abs(math.log10(a[i]) - b[i]) < 1e-3)
          
     def test_pow(self):
