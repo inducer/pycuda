92bcfd27ebc420ab5cf7fbcd87f61a49302f488c
wohlgemuth <wohlgemuth@ucdavis.edu>
2008-07-12 01:39:02 -0700

    just added wrapper for +/-/*/

--- src/python/cumath.py
+++ src/python/cumath.py
@@ -3,6 +3,23 @@ import pycuda.kernel as kernel
 import numpy
 import math
 
+def add(x,y):
+    """adds something"""
+    return x + y
+
+def substract(x,y):
+    """substract something"""
+    return x - y
+
+def multiply(x,y):
+    """multiply something"""
+    return x * y
+
+def divide(x,y):
+    """divides something"""
+    return x / y
+
+
 def ceil(array):
     """executes the ceil function on the gpu for all elements in the given array"""
     if isinstance(array, gpuarray.GPUArray):
