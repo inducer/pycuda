900f454186b3a80b28e6c4c8186b49b9cdbeffcc
Andreas Kloeckner <kloeckner@teramite.(none)>
2008-07-27 17:45:00 -0500

    Fix tests.

--- test/dump_properties.py
+++ test/dump_properties.py
@@ -6,7 +6,7 @@ drv.init()
 print "%d device(s) found." % drv.Device.count()
 
 for ordinal in range(drv.Device.count()):
-    dev = drv.Device(0)
+    dev = drv.Device(ordinal)
     print "Device #%d: %s" % (ordinal, dev.name())
     print "  Compute Capability: %d.%d" % dev.compute_capability()
     print "  Total Memory: %s KB" % (dev.total_memory()//(1024))
