399b6c1af02580bef858319b886734b14ab2aadf
Andreas Kloeckner <inform@tiker.net>
2008-11-27 12:09:11 -0500

    Bump version to 0.91.1 for previous fix, as reported by Matt Goodman.

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ def main():
 
     setup(name="pycuda",
             # metadata
-            version="0.91",
+            version="0.91.1",
             description="Python wrapper for Nvidia CUDA",
             long_description="""
             PyCuda lets you access `Nvidia <http://nvidia.com>`_'s `CUDA
