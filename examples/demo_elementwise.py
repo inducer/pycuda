55cafd258dbab6f8779af4fdf897dd67e104574c
Andreas Kloeckner <kloeckner@haamster.(none)>
2009-02-07 20:03:26 -0600

    Add pycuda.elementwise.ScalarKernel to make single-pass eval less clumsy.

--- examples/demo_elementwise.py
+++ examples/demo_elementwise.py
@@ -2,19 +2,19 @@ import pycuda.gpuarray as gpuarray
 import pycuda.driver as cuda
 import pycuda.autoinit
 import numpy
+from pycuda.curandom import rand as curand
 
-a_gpu = gpuarray.to_gpu(numpy.random.randn(50).astype(numpy.float32))
-b_gpu = gpuarray.to_gpu(numpy.random.randn(50).astype(numpy.float32))
+a_gpu = curand((50,))
+b_gpu = curand((50,))
 
-from pycuda.elementwise import get_scalar_kernel
-lin_comb = get_scalar_kernel(
+from pycuda.elementwise import ScalarKernel
+lin_comb = ScalarKernel(
         "float a, float *x, float b, float *y, float *z",
         "z[i] = a*x[i] + b*y[i]",
         "linear_combination")
 
 c_gpu = gpuarray.empty_like(a_gpu)
-lin_comb.set_block_shape(*a_gpu._block)
-lin_comb.prepared_call(a_gpu._grid, 5, a_gpu.gpudata, 6, b_gpu.gpudata, c_gpu.gpudata, a_gpu.mem_size)
+lin_comb(5, a_gpu, 6, b_gpu, c_gpu)
 
 import numpy.linalg as la
 assert la.norm((c_gpu - (5*a_gpu+6*b_gpu)).get()) < 1e-5
