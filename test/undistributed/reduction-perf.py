fa63c8acb7b7343a2d3d1578cfc40b8d0b2425bb
Andreas Kloeckner <kloeckner@hpcgeek.ccv.brown.edu>
2009-04-05 01:03:54 -0400

    Benchmark DP reduction, too.

--- test/undistributed/reduction-perf.py
+++ test/undistributed/reduction-perf.py
@@ -2,57 +2,59 @@ from __future__ import division
 import pycuda.autoinit
 import pycuda.gpuarray as gpuarray
 import pycuda.driver as cuda
+import numpy
 
 
 
 def main():
     from pytools import Table
     tbl = Table()
-    tbl.add_row(("size [MiB]", "time [ms]", "mem.bw [GB/s]"))
+    tbl.add_row(("type", "size [MiB]", "time [ms]", "mem.bw [GB/s]"))
 
     from random import shuffle
-    for ex in range(15,27):
-        sz = 1 << ex
-        print sz
+    for dtype_out in [numpy.float32, numpy.float64]:
+        for ex in range(15,27):
+            sz = 1 << ex
+            print sz
 
-        from pycuda.curandom import rand as curand
-        a_gpu = curand((sz,))
-        b_gpu = curand((sz,))
-        assert sz == a_gpu.shape[0]
-        assert len(a_gpu.shape) == 1
+            from pycuda.curandom import rand as curand
+            a_gpu = curand((sz,))
+            b_gpu = curand((sz,))
+            assert sz == a_gpu.shape[0]
+            assert len(a_gpu.shape) == 1
 
-        from pycuda.reduction import get_sum_kernel, get_dot_kernel
-        krnl = get_dot_kernel(a_gpu.dtype)
+            from pycuda.reduction import get_sum_kernel, get_dot_kernel
+            krnl = get_dot_kernel(dtype_out, a_gpu.dtype)
 
-        elapsed = [0]
+            elapsed = [0]
 
-        def wrap_with_timer(f):
-            def result(*args, **kwargs):
-                start = cuda.Event()
-                stop = cuda.Event()
-                start.record()
-                f(*args, **kwargs)
-                stop.record()
-                stop.synchronize()
-                elapsed[0] += stop.time_since(start)
+            def wrap_with_timer(f):
+                def result(*args, **kwargs):
+                    start = cuda.Event()
+                    stop = cuda.Event()
+                    start.record()
+                    f(*args, **kwargs)
+                    stop.record()
+                    stop.synchronize()
+                    elapsed[0] += stop.time_since(start)
 
-            return result
+                return result
 
-        # warm-up
-        for i in range(3):
-            krnl(a_gpu, b_gpu)
+            # warm-up
+            for i in range(3):
+                krnl(a_gpu, b_gpu)
 
-        cnt = 10
+            cnt = 10
 
-        for i in range(cnt):
-            krnl(a_gpu, b_gpu,
-            #krnl(a_gpu, 
-                    kernel_wrapper=wrap_with_timer)
+            for i in range(cnt):
+                krnl(a_gpu, b_gpu,
+                #krnl(a_gpu, 
+                        kernel_wrapper=wrap_with_timer)
 
-        bytes = a_gpu.nbytes*2*cnt
-        secs = elapsed[0]*1e-3
+            bytes = a_gpu.nbytes*2*cnt
+            secs = elapsed[0]*1e-3
 
-        tbl.add_row((a_gpu.nbytes/(1<<20), elapsed[0]/cnt, bytes/secs/1e9))
+            tbl.add_row((str(dtype_out), a_gpu.nbytes/(1<<20), elapsed[0]/cnt, bytes/secs/1e9))
 
     print tbl
 
