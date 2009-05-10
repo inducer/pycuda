d7fe3a806e6a2c271c046b022e8b596c5462d51e
Andreas Kloeckner <inform@tiker.net>
2009-05-09 20:46:47 -0400

    Bump version to 0.93rc1.

--- setup.py
+++ setup.py
@@ -111,7 +111,7 @@ def main():
 
     setup(name="pycuda",
             # metadata
-            version="0.93beta",
+            version="0.93rc1",
             description="Python wrapper for Nvidia CUDA",
             long_description="""
             PyCUDA lets you access `Nvidia <http://nvidia.com>`_'s `CUDA
