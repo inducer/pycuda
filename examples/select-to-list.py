1e54d346753591f6b5ea6a4a83bee1bff65d714d
HendrikRiedmann <riedmann@dam.brown.edu>
2009-05-19 18:33:33 -0400

    for everyone understandable version

--- examples/select-to-list.py
+++ examples/select-to-list.py
@@ -1,35 +1,42 @@
 # Exercise 2 from http://webapp.dam.brown.edu/wiki/SciComp/CudaExercises
 
 # Generate an array of random numbers between 0 and 1
-# List the indices of those numbers that are greater than 0.5
+# List the indices of those numbers that are greater than a given limit
 
 from __future__ import division
 import pycuda.driver as cuda
 import pycuda.autoinit
-import pycuda.gpuarray
+import pycuda.gpuarray as gpuarray
 from pycuda.compiler import SourceModule
 
 import numpy
 
 
-# Create the array of random numbers
+# Define block size and number of elements per thread
+
+block_size = 512
+el_per_thread = 1
+# !!! program only works for el_per_thread = 1            !!!
+# !!! otherwise the for-loops in the kernel cause a crash !!!
+multiple_block_size = el_per_thread * block_size
+
+
+# Create an array of random numbers and set limit
 
 amount = 256*2560
 limit = 0.9
-
+assert amount % multiple_block_size == 0
 a = numpy.random.rand(amount)
 a = a.astype(numpy.float32)
-a_gpu = cuda.mem_alloc(a.nbytes)
-cuda.memcpy_htod(a_gpu, a)
+a_gpu = gpuarray.to_gpu(a)
 
 
-# Initialize selec on device
+# Initialize array for the selection on device
 
 selec = numpy.zeros_like(a)
 selec = selec.astype(numpy.int32)
 selec.fill(-1)
-selec_gpu = cuda.mem_alloc(selec.nbytes)
-cuda.memcpy_htod(selec_gpu, selec)
+selec_gpu = gpuarray.to_gpu(selec)
 
 
 # Initialize a counter on device
@@ -43,30 +50,29 @@ cuda.memcpy_htod(counter_gpu, counter)
 # Computation on device
 
 mod = SourceModule("""
-#define BLOCK_SIZE 512
+#define BLOCK_SIZE %(block_size)d
+#define EL_PER_THREAD %(el_per_thread)d
 
 __global__ void select_them(float *a, int *selec, float limit, int *counter)
 {
-    __shared__ int selec_smem[4 * BLOCK_SIZE];
+    __shared__ int selec_smem[EL_PER_THREAD * BLOCK_SIZE];
     __shared__ int counter_smem;
     __shared__ int *counter_smem_ptr;
 
     int jump = 16;
-    int idx = 4 * blockIdx.x * BLOCK_SIZE + threadIdx.x + 
-              3 * (threadIdx.x / 16) * jump;
+    int idx = EL_PER_THREAD * blockIdx.x * BLOCK_SIZE + threadIdx.x + 
+              (EL_PER_THREAD - 1) * (threadIdx.x / 16) * jump;
 
     if (threadIdx.x == 1)
     {
         counter_smem_ptr = &counter_smem;
         counter_smem = 0;
     }
-    #if 0
+
+    #if EL_PER_THREAD == 1
         selec_smem[threadIdx.x] = -1;
-        selec_smem[threadIdx.x + BLOCK_SIZE] = -1;
-        selec_smem[threadIdx.x + 2 * BLOCK_SIZE] = -1;
-        selec_smem[threadIdx.x + 3 * BLOCK_SIZE] = -1;
     #else
-        for (int i = 0; i <= 3; i++)
+        for (int i = 0; i <= EL_PER_THREAD - 1; i++)
             selec_smem[threadIdx.x + i * BLOCK_SIZE] = -1;
     #endif
 
@@ -74,17 +80,11 @@ __global__ void select_them(float *a, int *selec, float limit, int *counter)
 
    // each counting thread writes its index to shared memory
 
-    #if 1
+    #if EL_PER_THREAD == 1
         if (a[idx] >= limit)
             selec_smem[atomicAdd(counter_smem_ptr, 1)] = idx;
-        if (a[idx + jump] >= limit)
-            selec_smem[atomicAdd(counter_smem_ptr, 1)] = idx + jump;
-        if (a[idx + 2 * jump] >= limit)
-            selec_smem[atomicAdd(counter_smem_ptr, 1)] = idx + 2 * jump;
-        if (a[idx + 3 * jump] >= limit)
-            selec_smem[atomicAdd(counter_smem_ptr, 1)] = idx + 3 * jump;
     #else
-         for (int i = 0; i <= 3; i++)
+         for (int i = 0; i <= EL_PER_THREAD - 1; i++)
          {
              if (a[idx + i * jump] >= limit)
                  selec_smem[atomicAdd(counter_smem_ptr, 1)] = idx + i * jump;
@@ -98,33 +98,19 @@ __global__ void select_them(float *a, int *selec, float limit, int *counter)
 
     __syncthreads();
 
-    #if 1
+    #if EL_PER_THREAD == 1
         if (selec_smem[threadIdx.x] >= 0)
             selec[counter_smem + threadIdx.x] = selec_smem[threadIdx.x];
-        if (selec_smem[threadIdx.x + jump] >= 0)
-            selec[counter_smem + threadIdx.x + jump] = 
-                selec_smem[threadIdx.x + jump];
-        if (selec_smem[threadIdx.x + 2 * jump] >= 0)
-            selec[counter_smem + threadIdx.x + 2 * jump] = 
-                selec_smem[threadIdx.x + 2 * jump];
-        if (selec_smem[threadIdx.x + 3 * jump] >= 0)
-            selec[counter_smem + threadIdx.x + 3 * jump] = 
-                selec_smem[threadIdx.x + 3 * jump];
     #else
-        for (int i = 0; i <= 3; i++)
+        for (int i = 0; i <= EL_PER_THREAD - 1; i++)
         {
             if (selec_smem[threadIdx.x + i * jump] >= 0)
                 selec[counter_smem + threadIdx.x + i * jump] = 
-	            selec_smem[threadIdx.x + i * jump];
+                selec_smem[threadIdx.x + i * jump];
         }
     #endif
 }
-""")
-
-# Define block size
-
-block_size = 512
-multiple_block_size = 4 * block_size
+""" % {"block_size": block_size, "el_per_thread": el_per_thread})
 
 
 # Prepare function call
@@ -133,13 +119,10 @@ func = mod.get_function("select_them")
 func.prepare("PPfP", block=(block_size, 1, 1))
 
 
-# Preparation for getting the time
+# Prepare getting the time
 
-start = pycuda.driver.Event()
-stop = pycuda.driver.Event()
-
-
-assert amount % multiple_block_size == 0
+start = cuda.Event()
+stop = cuda.Event()
 
 
 # Call function and get time
@@ -147,23 +130,23 @@ assert amount % multiple_block_size == 0
 cuda.Context.synchronize()
 start.record()
 func.prepared_call((amount // multiple_block_size, 1),
-                   a_gpu, selec_gpu, limit, counter_gpu)
+                   a_gpu.gpudata, selec_gpu.gpudata, limit, counter_gpu)
 stop.record()
 
 
 # Copy selection from device to host
 
-cuda.memcpy_dtoh(selec, selec_gpu)
+selec_gpu.get(selec)
+
+stop.synchronize()
 
 
-# Evaluate memory bandwidth
+# Evaluate memory bandwidth and verify solution
 
 elems_in_selec = 0
 for i in range(0, amount):
     if selec[i] >= 0:
         elems_in_selec = elems_in_selec + 1
-
-stop.synchronize()
 
 elapsed_seconds = stop.time_since(start) * 1e-3
 print "mem bw:", (a.nbytes + elems_in_selec * 4) / elapsed_seconds / 1e9
