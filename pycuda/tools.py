0219d4a65fedc41eac5d19c172768948d7a6515a
Andreas Kloeckner <inform@tiker.net>
2011-04-12 21:55:45 -0400

    Fix signed char arg parsing. (fix by Frédéric Bastien)

--- pycuda/tools.py
+++ pycuda/tools.py
@@ -473,7 +473,7 @@ def parse_c_arg(c_arg):
             dtype = np.uint32
     elif tp in ["short", "short int"]: dtype = np.int16
     elif tp in ["unsigned short", "unsigned short int"]: dtype = np.uint16
-    elif tp in ["char"]: dtype = np.int8
+    elif tp in ["char", "signed char"]: dtype = np.int8
     elif tp in ["unsigned char"]: dtype = np.uint8
     elif tp in ["bool"]: dtype = np.bool
     else:
