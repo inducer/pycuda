afa8f0e5dc9926c69279e393483fdd430b5607b0
Andreas Kloeckner <inform@tiker.net>
2011-08-29 17:44:20 +0200

    Remove config checking. Fix up default generation.

--- setup.py
+++ setup.py
@@ -2,41 +2,6 @@
 # -*- coding: latin-1 -*-
 
 
-def get_config_schema():
-    from aksetup_helper import ConfigSchema, Option, \
-            IncludeDir, LibraryDir, Libraries, BoostLibraries, \
-            Switch, StringListOption, make_boost_base_options
-
-    return ConfigSchema(make_boost_base_options() + [
-        Switch("USE_SHIPPED_BOOST", True, "Use included Boost library"),
-
-        BoostLibraries("python"),
-        BoostLibraries("thread"),
-
-        Switch("CUDA_TRACE", False, "Enable CUDA API tracing"),
-        Option("CUDA_ROOT", help="Path to the CUDA toolkit"),
-        Option("CUDA_PRETEND_VERSION", help="Assumed CUDA version, in the form 3010 for 3.1."),
-        IncludeDir("CUDA", None),
-
-        Switch("CUDA_ENABLE_GL", False, "Enable CUDA GL interoperability"),
-        Switch("CUDA_ENABLE_CURAND", True, "Enable CURAND library"),
-
-        LibraryDir("CUDADRV", []),
-        Libraries("CUDADRV", ["cuda"]),
-
-        LibraryDir("CUDART", ["${CUDA_ROOT}/lib", "${CUDA_ROOT}/lib64"]),
-        Libraries("CUDART", ["cudart"]),
-
-        LibraryDir("CURAND", ["${CUDA_ROOT}/lib", "${CUDA_ROOT}/lib64"]),
-        Libraries("CURAND", ["curand"]),
-
-        StringListOption("CXXFLAGS", [],
-            help="Any extra C++ compiler options to include"),
-        StringListOption("LDFLAGS", [],
-            help="Any extra linker options to include"),
-        ])
-
-
 def search_on_path(filenames):
     """Find file on system path."""
     # http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/52224
@@ -52,156 +17,51 @@ def search_on_path(filenames):
             if exists(join(path, filename)):
                 return abspath(join(path, filename))
 
+def get_config_schema():
+    from aksetup_helper import ConfigSchema, Option, \
+            IncludeDir, LibraryDir, Libraries, BoostLibraries, \
+            Switch, StringListOption, make_boost_base_options
 
-# verification ----------------------------------------------------------------
-def verify_path(description, paths, names, extensions, subpaths=['/'],
-        prefixes=[], maybe_ok=False):
-    try:
-        from os.path import exists
+    nvcc_path = search_on_path(["nvcc", "nvcc.exe"])
+    if nvcc_path is None:
+        print("*** CUDA_ROOT not set, and nvcc not in path. Giving up.")
+        sys.exit(1)
 
-        prefixes.append("")
-        looked_where = []
+    cuda_root_default = normpath(join(dirname(nvcc_path), ".."))
 
-        for path in paths:
-            for subpath in subpaths:
-                for prefix in prefixes:
-                    for name in names:
-                        for extension in extensions:
-                            print(path, subpath, prefix, name, extension)
-                            filename = path + subpath + prefix + name + extension
+    return ConfigSchema(make_boost_base_options() + [
+        Switch("USE_SHIPPED_BOOST", True, "Use included Boost library"),
 
-                            looked_where.append(filename)
+        BoostLibraries("python"),
+        BoostLibraries("thread"),
 
-                            if exists(filename):
-                                return
-        print("*** Cannot find %s. Checked locations:" % description)
-        for path in looked_where:
-            print("   %s" % path)
+        Switch("CUDA_TRACE", False, "Enable CUDA API tracing"),
+        Option("CUDA_ROOT", default=cuda_root_default, help="Path to the CUDA toolkit"),
+        Option("CUDA_PRETEND_VERSION", help="Assumed CUDA version, in the form 3010 for 3.1."),
+        IncludeDir("CUDA", None),
 
-        if maybe_ok:
-            print("*** Note that this may not be a problem as this "
-                    "component is often installed system-wide.")
-    except:
-        print("*** Error occurred in plausibility checking for path of %s."
-                % description)
+        Switch("CUDA_ENABLE_GL", False, "Enable CUDA GL interoperability"),
+        Switch("CUDA_ENABLE_CURAND", True, "Enable CURAND library"),
 
+        LibraryDir("CUDADRV", ["${CUDA_ROOT}/lib", "${CUDA_ROOT}/lib64"]),
+        Libraries("CUDADRV", ["cuda"]),
 
+        LibraryDir("CUDART", ["${CUDA_ROOT}/lib", "${CUDA_ROOT}/lib64"]),
+        Libraries("CUDART", ["cudart"]),
 
+        LibraryDir("CURAND", ["${CUDA_ROOT}/lib", "${CUDA_ROOT}/lib64"]),
+        Libraries("CURAND", ["curand"]),
 
-def verify_siteconfig(sc_vars):
-    LIB_EXTS = ['.so', '.dylib', '.lib']
-    LIB_PREFIXES = ['lib']
-    APP_EXTS = ['', '.exe']
-    warn_prefix = '!! Warning: '
-
-    # BOOST_INC_DIR/boost/python.hpp
-    if 'BOOST_INC_DIR' in sc_vars:
-        verify_path (
-            description="Boost headers",
-            paths=sc_vars['BOOST_INC_DIR'],
-            subpaths=['/boost/'],
-            names=['python'],
-            extensions=['.hpp']
-            );
-    else:
-        print(warn_prefix + 'BOOST_INC_DIR is not set, should be something like '
-                '"/path/to/boost/include/boost-1_39".')
-
-    # BOOST_LIB_DIR/(lib)?BOOST_PYTHON_LIBNAME(.so|.dylib|?Windows?)
-    if 'BOOST_LIB_DIR' not in sc_vars:
-        print(warn_prefix + 'BOOST_LIB_DIR is not set, should be '
-                'like BOOST_INC_DIR but with "/lib" instead of '
-                '"/include/boost-1_39".')
-
-    if 'BOOST_PYTHON_LIBNAME' in sc_vars:
-        verify_path (
-            description="Boost Python library",
-            paths=sc_vars['BOOST_LIB_DIR'],
-            names=sc_vars['BOOST_PYTHON_LIBNAME'],
-            extensions=LIB_EXTS,
-            prefixes=LIB_PREFIXES
-            )
-    else:
-        print(warn_prefix + 'BOOST_PYTHON_LIBNAME is not set, '
-                'should be something like "boost_python-*-mt".')
-
-    # BOOST_LIB_DIR/(lib)?BOOST_THREAD_LIBNAME(.so|.dylib|?Windows?)
-    if 'BOOST_THREAD_LIBNAME' in sc_vars:
-        verify_path(
-            description="Boost Thread library",
-            paths=sc_vars['BOOST_LIB_DIR'],
-            names=sc_vars['BOOST_THREAD_LIBNAME'],
-            extensions=LIB_EXTS,
-            prefixes=LIB_PREFIXES
-            )
-    else:
-        print(warn_prefix + 'BOOST_THREAD_LIBNAME is not set, '
-                'should be something like "boost_thread-*-mt".')
-
-    # CUDA_ROOT/bin/nvcc(.exe)?
-    if 'CUDA_ROOT' in sc_vars:
-        verify_path(
-            description="CUDA toolkit",
-            paths=[sc_vars['CUDA_ROOT']],
-            subpaths=['/bin/'],
-            names=['nvcc'],
-            extensions=APP_EXTS,
-            )
-    else:
-        print(warn_prefix + 'CUDA_ROOT is not set, '
-                'should point to the nVidia CUDA Toolkit.')
-
-    # CUDA_INC_DIR/cuda.h
-    if 'CUDA_INC_DIR' in sc_vars:
-        verify_path (
-            description="CUDA include directory",
-            paths=sc_vars['CUDA_INC_DIR'],
-            names=['cuda'],
-            extensions=['.h'],
-            )
-    else:
-        print(warn_prefix + 'CUDA_INC_DIR is not set, '
-                'should be something like CUDA_ROOT + "/include".')
-
-    # CUDADRV_LIB_DIR=(lib)?CUDADRV_LIBNAME(.so|.dylib|?Windows?)
-    if not 'CUDADRV_LIB_DIR' in sc_vars:
-        print(warn_prefix + 'CUDADRV_LIB_DIR is not set, should '
-                'be something like CUDA_ROOT + "/lib".')
-
-    if 'CUDADRV_LIBNAME' in sc_vars:
-        verify_path (
-            description="CUDA driver library",
-            paths=sc_vars['CUDADRV_LIB_DIR'],
-            names=sc_vars['CUDADRV_LIBNAME'],
-            extensions=LIB_EXTS,
-            prefixes=LIB_PREFIXES,
-            maybe_ok=True,
-            )
-    else:
-        print(warn_prefix + 'CUDADRV_LIBNAME is not set, should most likely be "cuda".')
-
-    # CUDART_LIB_DIR=(lib)?CUDART_LIBNAME(.so|.dylib|?Windows?)
-    if not 'CUDART_LIB_DIR' in sc_vars:
-        # Since libcurand.so is distributed in the CUDA Runtime lib directory.
-        # which is different from the CUDA Driver lib directory, we should check.
-        print(warn_prefix + 'CUDART_LIB_DIR is not set, should '
-                'be sometthing like CUDA_ROOT + "/lib".')
-
-    if 'CUDART_LIBNAME' in sc_vars:
-        verify_path (
-            description="CUDA runtime library",
-            paths=sc_vars['CUDART_LIB_DIR'],
-            names=sc_vars['CUDART_LIBNAME'],
-            extensions=LIB_EXTS,
-            prefixes=LIB_PREFIXES,
-            maybe_ok=True,
-            )
-    else:
-        # This should be optional, since PyCUDA doesn't directly use the CUDART
-        pass
+        StringListOption("CXXFLAGS", [],
+            help="Any extra C++ compiler options to include"),
+        StringListOption("LDFLAGS", [],
+            help="Any extra linker options to include"),
+        ])
 
 
-# main functionality ----------------------------------------------------------
+
+
+
 def main():
     import glob
     import sys
@@ -224,28 +84,8 @@ def main():
     LIBRARIES = (conf["BOOST_PYTHON_LIBNAME"] + conf["BOOST_THREAD_LIBNAME"]
             + conf["CUDADRV_LIBNAME"])
 
-    if conf["CUDA_ROOT"] is None:
-        nvcc_path = search_on_path(["nvcc", "nvcc.exe"])
-        if nvcc_path is None:
-            print("*** CUDA_ROOT not set, and nvcc not in path. Giving up.")
-            sys.exit(1)
-
-        conf["CUDA_ROOT"] = normpath(join(dirname(nvcc_path), ".."))
-
     if not conf["CUDA_INC_DIR"]:
         conf["CUDA_INC_DIR"] = [join(conf["CUDA_ROOT"], "include")]
-
-    if not conf["CUDADRV_LIB_DIR"]:
-        platform_bits = tuple.__itemsize__ * 8
-
-        if platform_bits == 64 and 'darwin' not in sys.platform:
-            lib_dir_name = "lib64"
-        else:
-            lib_dir_name = "lib"
-
-        conf["CUDADRV_LIB_DIR"] = [join(conf["CUDA_ROOT"], lib_dir_name)]
-
-    verify_siteconfig(conf)
 
     if conf["CUDA_TRACE"]:
         EXTRA_DEFINES["CUDAPP_TRACE_CUDA"] = 1
