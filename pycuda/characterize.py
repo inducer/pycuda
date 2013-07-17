0417d9531227844863d7f901431d145f0ff29421
Andreas Kloeckner <inform@tiker.net>
2013-07-17 13:29:42 -0400

    PEP8 pycuda.characterize

--- pycuda/characterize.py
+++ pycuda/characterize.py
@@ -4,27 +4,18 @@ from pycuda.tools import context_dependent_memoize
 import numpy as np
 
 
-
-
-
 def platform_bits():
     return tuple.__itemsize__ * 8
 
 
-
-
 def has_stack():
     from pycuda.driver import Context
-    return Context.get_device().compute_capability() >= (2,0)
-
-
+    return Context.get_device().compute_capability() >= (2, 0)
 
 
 def has_double_support():
     from pycuda.driver import Context
-    return Context.get_device().compute_capability() >= (1,3)
-
-
+    return Context.get_device().compute_capability() >= (1, 3)
 
 
 @context_dependent_memoize
@@ -41,7 +32,6 @@ def sizeof(type_name, preamble=""):
 
     import pycuda.gpuarray as gpuarray
     output = gpuarray.empty((), dtype=np.uintp)
-    mod.get_function("write_size")(output, block=(1,1,1), grid=(1,1))
+    mod.get_function("write_size")(output, block=(1, 1, 1), grid=(1, 1))
 
     return int(output.get())
-
