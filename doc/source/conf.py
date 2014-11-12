0758692681635cb83d8771eba1b4698256c53a55
Andreas Kloeckner <inform@tiker.net>
2014-11-12 15:23:45 -0600

    Documentation config fixes

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,7 +23,10 @@
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.intersphinx']
+extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
