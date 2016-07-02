f35537266787ab9bca2f19081a955b1a6670578f
Andreas Kloeckner <inform@tiker.net>
2016-07-01 20:36:04 -0500

    Example downloader tweaks

--- examples/download-examples-from-wiki.py
+++ examples/download-examples-from-wiki.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python
-from __future__ import print_function
-from __future__ import absolute_import
+from __future__ import print_function, absolute_import
 try:
     import six.moves.xmlrpc_client
 except:
