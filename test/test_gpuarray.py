cfb787ac73a523fe4b32eff31ecffac485388bbf
Andreas Kloeckner <inform@tiker.net>
2017-11-30 16:38:58 -0600

    Fix test_dot_allocator_test skipping

--- test/test_gpuarray.py
+++ test/test_gpuarray.py
@@ -952,7 +952,9 @@ class TestGPUArray:
 
     @mark_cuda_test
     def test_dot_allocator(self):
-        pytest.skip("https://github.com/inducer/pycuda/issues/163")
+        from pytest import skip
+        skip("https://github.com/inducer/pycuda/issues/163")
+
         import pycuda.tools
         pool = pycuda.tools.DeviceMemoryPool()
 
