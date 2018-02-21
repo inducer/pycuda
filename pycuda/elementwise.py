edcc44af0dd5d8bc65ffe5672ee9c0e63438162c
Syam Gadde <gadde@biac.duke.edu>
2018-02-21 14:36:40 -0500

    Forgot to remove non-contiguity check.

--- pycuda/elementwise.py
+++ pycuda/elementwise.py
@@ -520,10 +520,6 @@ class ElementwiseKernel:
 
         for arg, arg_descr in zip(args, arguments):
             if isinstance(arg_descr, VectorArg):
-                if not arg.flags.forc:
-                    raise RuntimeError("elementwise kernel cannot "
-                            "deal with non-contiguous arrays")
-
                 vectors.append(arg)
                 invocation_args.append(arg)
             else:
