e2c95023a9fd01709d4054aee2f22944a214b1c0
mit kotak <mitak2@illinois.edu>
2021-07-17 16:43:39 -0500

    Implements gpuarray.(stack|concatenate)

--- pycuda/gpuarray.py
+++ pycuda/gpuarray.py
@@ -1236,11 +1236,6 @@ def to_gpu_async(ary, allocator=drv.mem_alloc, stream=None):
 
 empty = GPUArray
 
-def empty(shape, dtype, allocator=drv.mem_alloc, order="C"):
-    """Returns an array of the given shape and dtype which is empty."""
-    result = GPUArray(shape, dtype, allocator=allocator, order=order)
-    return result
-
 
 def zeros(shape, dtype, allocator=drv.mem_alloc, order="C"):
     """Returns an array of the given shape and dtype filled with 0's."""
@@ -1783,14 +1778,11 @@ def concatenate(arrays, axis=0, allocator=None):
         Can be -1, for the new axis to be last dimension.
     :returns: :class:`GPUArray`
     """
-
     shape = None
-
     def shape_except_axis(ary: GPUArray):
         return ary.shape[:axis] + ary.shape[axis+1:]
 
-    for i_ary, ary in enumerate(arrays[1:]):
-        
+    for i_ary, ary in enumerate(arrays):
         allocator = allocator or ary.allocator
 
         if shape is None:
@@ -1803,15 +1795,14 @@ def concatenate(arrays, axis=0, allocator=None):
                         "(shold have %d, has %d)"
                         % (i_ary, len(ary.shape), len(shape)))
 
-                if (array.ndim != arrays[0].ndim
-                        or shape_except_axis(array) != shape_except_axis(arrays[0])):
-                    raise ValueError("%d'th array has residual not matching "
-                            "other arrays" % i_ary)
+            if (ary.ndim != arrays[0].ndim
+                    or shape_except_axis(ary) != shape_except_axis(arrays[0])):
+                raise ValueError("%d'th array has residual not matching "
+                        "other arrays" % i_ary)
 
-                shape[axis] += ary.shape[axis]
+            shape[axis] += ary.shape[axis]
 
     # }}}
-
     shape = tuple(shape)
     dtype = np.find_common_type([ary.dtype for ary in arrays], [])
     result = empty(shape, dtype, allocator=allocator)
@@ -1821,32 +1812,31 @@ def concatenate(arrays, axis=0, allocator=None):
     base_idx = 0
     for ary in arrays:
         my_len = ary.shape[axis]
-        result[full_slice[:axis] + (slice(base_idx, base_idx+my_len),) + full_slice[axis+1:] ] = ary
+        result[full_slice[:axis] + (slice(base_idx, base_idx+my_len),) + full_slice[axis+1:]] = ary
         base_idx += my_len
 
     return result
 
 
-
 def stack(arrays, axis=0, allocator=None):
-    #impllementation is borrowed from pyopencl.array.stack()
     """
+    implementation is borrowed from pyopencl.array.stack()
+
     Join a sequence of arrays along a new axis.
-    
     :arg arrays: A sequnce of :class:`GPUArray`.
     :arg axis: Index of the dimension of the new axis in the result array.
         Can be -1, for the new axis to be last dimension.
     :returns: :class:`GPUArray`
     
     """
-    
+    allocator = allocator or arrays[0].allocator  
+
     if not arrays:
         raise ValueError("need at least one array to stack")
 
     input_shape = arrays[0].shape
     input_ndim = arrays[0].ndim
     axis = input_ndim if axis == -1 else axis
-
 
     if not all(ary.shape == input_shape for ary in arrays[1:]):
         raise ValueError("arrays must have the same shape")
@@ -1856,7 +1846,7 @@ def stack(arrays, axis=0, allocator=None):
 
     if (axis == 0 and not all(ary.flags.c_contiguous
                                        for ary in arrays)):
-         # pycuda.GPUArray.__setitem__ does not support non-contiguous assignments
+        # pycuda.GPUArray.__setitem__ does not support non-contiguous assignments
         raise NotImplementedError
 
     if (axis == input_ndim and not all(ary.flags.f_contiguous
@@ -1864,18 +1854,16 @@ def stack(arrays, axis=0, allocator=None):
         # pycuda.Array.__setitem__ does not support non-contiguous assignments
         raise NotImplementedError
 
-
     result_shape = input_shape[:axis] + (len(arrays),) + input_shape[axis:]
-
     
-    result = empty(shape = result_shape, dtype = np.result_type(*(ary.dtype for ary in arrays)), allocator = allocator, order="C" if axis == 0 else "F")
+    result = empty(shape=result_shape, dtype = np.result_type(*(ary.dtype for ary in arrays)), allocator = allocator, order = "C" if axis == 0 else "F")
 
     for i, ary in enumerate(arrays):
+
         idx = (slice(None),)*axis + (i,) + (slice(None),)*(input_ndim-axis)
         result[idx] = ary
 
     return result
-
 
 
 def transpose(a, axes=None):
