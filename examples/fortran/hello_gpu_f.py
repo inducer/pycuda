3de1969ee56aff0ef6b47cc3f4bc1360d0139d11
Kevin Schmidt <w9cf@arrl.net>
2025-11-07 12:28:49 -0700

    Removed C version of cuda code

--- examples/fortran/hello_gpu_f.py
+++ examples/fortran/hello_gpu_f.py
@@ -6,14 +6,6 @@ import numpy.linalg as la
 from pycuda.compiler import SourceModule
 
 mod = SourceModule("""
-__global__ void multiply_them(float *dest, float *a, float *b)
-{
-  const int i = threadIdx.x;
-  dest[i] = a[i] * b[i];
-}
-""")
-
-mod = SourceModule("""
    attributes (global) subroutine multiply_them(dest,a,b) bind(c)
    use, intrinsic :: iso_fortran_env
    implicit none
